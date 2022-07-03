d0e616e7e9bd63f33973bebf03fb9014c564f949
zmrl <zmriel@gmail.com>
2022-07-03 18:42:49 -0400

    fix: typo preventing packer install

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,5 @@
 local packer_repo_url = 'https://github.com/wbthomason/packer.vim'
-local install_path = vim.fn.stdpath('data')..'site/pack/packer/start/pack.nvim'
+local install_path = vim.fn.stdpath('data')..'site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({
     'git',
